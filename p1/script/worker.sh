090c63c52d7cbe0594b0979f7ad0fbd0511c8f87
hugo <hlefevr@hugo.42nice.fr>
2025-08-06 09:29:26 -0400

    fix(p1): hulefevr: Added a reset-token in the makefile in case of a desync. P1 is finished

--- p1/script/worker.sh
+++ p1/script/worker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MASTER_IP="$1"
+
 echo "[INFO] Waiting for node-token from controller..."
 while [ ! -f /vagrant_shared/node-token ]; do
   echo "[INFO] node-token not found yet, sleeping 2s..."
@@ -9,5 +11,5 @@ done
 K3S_TOKEN=$(cat /vagrant_shared/node-token)
 
 echo "[INFO] Installing K3s agent on $HOSTNAME..."
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="$K3S_TOKEN" sh -
+curl -sfL https://get.k3s.io | K3S_URL="https://${MASTER_IP}:6443" K3S_TOKEN="$K3S_TOKEN" sh -
 echo "[INFO] K3s agent installed on $HOSTNAME."
